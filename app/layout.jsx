c86e6b665f3cb438f6e0722f924b3c09873a6f79
Zuha <zuhasohail2003@gmail.com>
2024-07-28 22:02:26 +0500

    promptopia-basics

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css'
+
+export const metadata={
+    title:'Promptopia',
+    description:'Discover amd share AI prompts'
+}
+
+
+function layout() {
+  return (
+    <html>
+        <body>
+            <div className="main">
+                <div className="gradient">
+                    
+                </div>
+            </div>
+        </body>
+    </html>
+  )
+}
+
+export default layout
