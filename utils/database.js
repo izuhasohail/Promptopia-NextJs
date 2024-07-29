b159cdc78aa09c4532a8acb33d62647306ba3619
Zuha <zuhasohail2003@gmail.com>
2024-07-30 00:19:32 +0500

    db

---
+++ utils/database.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+let isConnected=false;
+
+
+
+export const connectedToDB= async()=>{
+    mongoose.set('strictQuery',true)
+
+    if (isConnected){
+         console.log('Already connected to MongoDB')
+         return;
+
+    }
+
+    try{
+        await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:"share_prompts",
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        })
+
+        isConnected=true
+        console.log('Connected to MongoDB')
+    }
+    catch(error){
+             console.log(error)
+    }
+
+}
